15b180ad3b97a3edd0b7aab9cbf98fe00154af13
Joshua Waheed <joshua.waheed@gmail.com>
2022-11-13 13:47:13 +0000

    Initialise project

---
+++ ExpensesApp/ExpensesApp/Views/ExpensesPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ExpensesApp.Views
+{
+    public partial class ExpensesPage : ContentPage
+    {
+        public ExpensesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
