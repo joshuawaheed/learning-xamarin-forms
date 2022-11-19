cde4f35654752a587b57273c373d1daceae2a11b
Joshua Waheed <joshua.waheed@gmail.com>
2022-11-19 17:50:27 +0000

    Implement the Android Effect

--- ExpensesApp/ExpensesApp.Android/Effects/SelectedEffect.cs
+++ ExpensesApp/ExpensesApp.Android/Effects/SelectedEffect.cs
@@ -1,4 +1,7 @@
-﻿using ExpensesApp.Droid.Effects;
+﻿using System;
+using System.ComponentModel;
+using Android.Graphics.Drawables;
+using ExpensesApp.Droid.Effects;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
@@ -8,18 +11,40 @@ namespace ExpensesApp.Droid.Effects
 {
     public class SelectedEffect : PlatformEffect
 	{
-		public SelectedEffect()
-		{
-		}
+        Android.Graphics.Color selectedColor;
 
         protected override void OnAttached()
         {
-            
+            selectedColor = new Android.Graphics.Color(176, 152, 164);
         }
 
         protected override void OnDetached()
         {
             
+        }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            try
+            {
+                if (args.PropertyName == "IsFocused")
+                {
+                    if (((ColorDrawable)Control.Background).Color != selectedColor)
+                    {
+                        Control.SetBackgroundColor(selectedColor);
+                    }
+                    else
+                    {
+                        Control.SetBackgroundColor(Android.Graphics.Color.White);
+                    }
+                }
+            }
+            catch (InvalidCastException)
+            {
+                Control.SetBackgroundColor(selectedColor);
+            }
         }
     }
 }
