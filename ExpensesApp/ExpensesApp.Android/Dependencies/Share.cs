a26720e1f4dea474862e4908e076cc4753a31da5
Joshua Waheed <joshua.waheed@gmail.com>
2022-11-19 13:21:25 +0000

    Sharing a file in Android

--- ExpensesApp/ExpensesApp.Android/Dependencies/Share.cs
+++ ExpensesApp/ExpensesApp.Android/Dependencies/Share.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using Android.Content;
+using AndroidX.Core.Content;
 using ExpensesApp.Droid.Dependencies;
 using ExpensesApp.Interfaces;
 using Xamarin.Forms;
@@ -8,9 +10,25 @@ namespace ExpensesApp.Droid.Dependencies
 {
     public class Share : IShare
     {
-        public async Task Show(string title, string message, string filePath)
+        public Task Show(string title, string message, string filePath)
         {
-            
+            var intent = new Intent(Intent.ActionSend);
+            intent.SetType("text/plain");
+
+            var documentUriContext = Forms.Context.ApplicationContext;
+            var documentUriAuthority = "com.joshuawaheed.expensesapp.provider";
+            var documentUriFile = new Java.IO.File(filePath);
+            var documentUri = FileProvider.GetUriForFile(documentUriContext, documentUriAuthority, documentUriFile);
+
+            intent.PutExtra(Intent.ExtraStream, documentUri);
+            intent.PutExtra(Intent.ExtraText, title);
+            intent.PutExtra(Intent.ExtraSubject, message);
+
+            var chooserIntent = Intent.CreateChooser(intent, title);
+            chooserIntent.SetFlags(ActivityFlags.GrantReadUriPermission);
+            Android.App.Application.Context.StartActivity(chooserIntent);
+
+            return Task.FromResult(true);
         }
     }
 }
