1fd7e373cdd4fa64cea0c726908ce7a3aca6a744
Joshua Waheed <joshua.waheed@gmail.com>
2022-11-19 18:02:55 +0000

    Implement the Effect for iOS

--- ExpensesApp/ExpensesApp.iOS/Effects/SelectedEffect.cs
+++ ExpensesApp/ExpensesApp.iOS/Effects/SelectedEffect.cs
@@ -1,4 +1,6 @@
-﻿using ExpensesApp.iOS.Effects;
+﻿using System.ComponentModel;
+using ExpensesApp.iOS.Effects;
+using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 
@@ -8,18 +10,33 @@ namespace ExpensesApp.iOS.Effects
 {
     public class SelectedEffect : PlatformEffect
 	{
-		public SelectedEffect()
-		{
-		}
+		UIColor selectedColor;
 
         protected override void OnAttached()
         {
-            
+            selectedColor = new UIColor(176.0f/255.0f, 152.0f/255.0f, 164.0f/255.0f, 255.0f/255.0f);
         }
 
         protected override void OnDetached()
         {
             
+        }
+
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            if (args.PropertyName == "IsFocused")
+            {
+                if (Control.BackgroundColor != selectedColor)
+                {
+                    Control.BackgroundColor = selectedColor;
+                }
+                else
+                {
+                    Control.BackgroundColor = UIColor.White;
+                }
+            }
         }
     }
 }
