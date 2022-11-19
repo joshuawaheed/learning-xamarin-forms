e644385bd6e0278391f7bd899dc7cc344217055e
Joshua Waheed <joshua.waheed@gmail.com>
2022-11-19 17:16:16 +0000

    Implementing effects

---
+++ ExpensesApp/ExpensesApp.Android/Effects/SelectedEffect.cs
@@ -0,0 +1,26 @@
+﻿using ExpensesApp.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("LPA")]
+[assembly: ExportEffect(typeof(SelectedEffect), nameof(SelectedEffect))]
+namespace ExpensesApp.Droid.Effects
+{
+    public class SelectedEffect : PlatformEffect
+	{
+		public SelectedEffect()
+		{
+		}
+
+        protected override void OnAttached()
+        {
+            
+        }
+
+        protected override void OnDetached()
+        {
+            
+        }
+    }
+}
+
