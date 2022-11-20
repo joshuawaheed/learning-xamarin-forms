791d8ef3b136ab4f4906103d2cb0d702053f3be8
Joshua Waheed <joshua.waheed@gmail.com>
2022-11-20 08:31:07 +0000

    Initialise project

---
+++ FinanceApp/FinanceApp/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FinanceApp.Views
+{	
+	public partial class MainPage : ContentPage
+	{	
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
