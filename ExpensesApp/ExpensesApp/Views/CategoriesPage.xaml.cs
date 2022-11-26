c8ec76d2cd6907cf6ae86540fb72d888f318be39
Joshua Waheed <joshua.waheed@gmail.com>
2022-11-26 12:44:35 +0000

    Utilise the VisualStateManager

--- ExpensesApp/ExpensesApp/Views/CategoriesPage.xaml.cs
+++ ExpensesApp/ExpensesApp/Views/CategoriesPage.xaml.cs
@@ -14,6 +14,8 @@ namespace ExpensesApp.Views
             InitializeComponent();
 
             ViewModel = Resources["vm"] as CategoriesViewModel;
+
+            SizeChanged += CategoriesPage_SizeChanged;
         }
 
         protected override void OnAppearing()
@@ -21,6 +23,23 @@ namespace ExpensesApp.Views
             base.OnAppearing();
 
             ViewModel.GetExpensesPerCategory();
+        }
+
+        private void CategoriesPage_SizeChanged(object sender, EventArgs e)
+        {
+            string visualState = Width > Height ? "Landscape" : "Portrait";
+
+            VisualStateManager.GoToState(titleLabel, visualState);
+        }
+
+        private void ExampleButton_Pressed(System.Object sender, System.EventArgs e)
+        {
+            VisualStateManager.GoToState(exampleButton, "Focused");
+        }
+
+        private void ExampleButton_Released(System.Object sender, System.EventArgs e)
+        {
+            VisualStateManager.GoToState(exampleButton, "Normal");
         }
     }
 }
